35cc1b8d72ef574bda83221a88108f199d0d1526
Piotr Borowski <piotrborowski1997@gmail.com>
2024-03-21 13:58:32 +0100

    add button

--- components/NastyComponent.js
+++ components/NastyComponent.js
@@ -1,8 +1,12 @@
 import { useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 
-export const NastyComponent = () => {
+export const NastyComponent = ({ initialValue }) => {
   const [state, setState] = useState(0);
+
+  const someFunction = () => {
+    setState((prev) => prev + 1);
+  };
 
   useEffect(() => {
     setState(state + 1);
@@ -11,6 +15,7 @@ export const NastyComponent = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app! {state}</Text>
+      <Button title="Press me" onPress={someFunction} />
     </View>
   );
 };
